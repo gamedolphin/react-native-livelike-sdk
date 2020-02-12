26d284001434426c57d952bdf822b20a914dfd95
Sandeep Nambiar <sandeep@livelikevr.com>
2020-02-12 10:31:39 +0530

    Setup min height to 1000 so that widget has space

--- android/src/main/java/com/livelike/rnsdk/util/ViewUtils.kt
+++ android/src/main/java/com/livelike/rnsdk/util/ViewUtils.kt
@@ -1,16 +1,17 @@
 package com.livelike.rnsdk.util
 
 import android.content.Context
+import kotlin.math.roundToInt
 
 object ViewUtils {
 
     fun dpToPx(context: Context, dp: Int): Int {
         val density = context.resources.displayMetrics.density
-        return Math.round(dp.toFloat() * density)
+        return (dp.toFloat() * density).roundToInt()
     }
 
     fun pxToDp(context: Context, px: Int): Int {
         val density = context.resources.displayMetrics.density
-        return Math.round(px.toFloat() / density)
+        return (px.toFloat() / density).roundToInt()
     }
 }
--- example/App.js
+++ example/App.js
@@ -74,7 +74,7 @@ const App = () => {
       <View
         style={{
           height: widgetHeight,
-          width: '100%',
+          width: "100%",
           backgroundColor: "black",
           overflow: "hidden",
           justifyContent: "flex-start",
@@ -83,7 +83,7 @@ const App = () => {
         }}
       >
         <View
-          style={{ minHeight: 126, width: "100%", backgroundColor: "green" }}
+          style={{ minHeight: 1000, width: "100%", backgroundColor: "green" }}
         >
           {initialized ? (
             <LivelikeWidgetView
